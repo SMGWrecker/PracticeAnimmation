202bd0599ba7d44fea9f7a2e85bd299a80f3d43b
Test <Test>
2020-12-07 06:25:51 +0300

    Animation

---
+++ PracticeAnimmation/CandyViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CandyViewController.swift
+//  PracticeAnimmation
+//
+//  Created by user on 06/12/2020.
+//
+
+import UIKit
+
+class CandyViewController: UIViewController {
+
+    @IBOutlet weak var candyPicture: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: [.repeat, .autoreverse], animations: {
+            self.candyPicture.frame.size.width = 105
+            self.candyPicture.frame.size.height = 54
+        }, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+}
---
+++ PracticeAnimmation/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  PracticeAnimmation
+//
+//  Created by user on 06/12/2020.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var inputLogin: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var logotype: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        inputLogin.center.x -= view.bounds.width
+        inputPassword.center.x += view.bounds.width
+        appNameLabel.center.y -= view.bounds.height
+        logotype.center.y -= view.bounds.height
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0) {
+            self.inputLogin.center.x += self.view.bounds.width
+            self.inputPassword.center.x -= self.view.bounds.width
+            self.appNameLabel.center.y += self.view.bounds.height
+            self.logotype.center.y += self.view.bounds.height
+        }
+    }
+    
+
+}
